a9c97985f92b04069fcebccd03bd4d0f4e163755
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2021-08-10 08:57:50 +0900

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -4,9 +4,14 @@ def mapping(x,in_min,in_max,out_min,out_max):
 '''
 mapping(value,fromLow,fromHigh,toLow,toHigh)
 
-value: 変換したい数値
-fromLow: 現在の範囲の下限
-fromHigh: 現在の範囲の上限
-toLow: 変換後の範囲の下限
-toHigh: 変換後の範囲の上限
+value: 変換したい数値(変数)/Value to convert(variable)
+fromLow: 現在の範囲の下限/minimum of currently range
+fromHigh: 現在の範囲の上限/maximum of currently range
+toLow: 変換後の範囲の下限/minimum of converted range
+toHigh: 変換後の範囲の上限/maximum of converted range
+
+*example*
+import mapping
+x=50
+mapping.mapping(x,0,100,80,255)
 '''

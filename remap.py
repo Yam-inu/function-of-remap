8f7add79402307a920343e181d05a32b4f6354dc
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2021-08-10 09:10:25 +0900

    Update and rename mapping.py to remap.py

--- remap.py
+++ remap.py
@@ -1,8 +1,8 @@
-def mapping(x,in_min,in_max,out_min,out_max):
+def remap(x,in_min,in_max,out_min,out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 '''
-mapping(value,fromLow,fromHigh,toLow,toHigh)
+remap(value,fromLow,fromHigh,toLow,toHigh)
 
 value: 変換したい数値(変数)/Value to convert(variable)
 fromLow: 現在の範囲の下限/minimum of currently range
@@ -11,7 +11,7 @@ toLow: 変換後の範囲の下限/minimum of converted range
 toHigh: 変換後の範囲の上限/maximum of converted range
 
 *example*
-import mapping
+import remap
 x=50
-mapping.mapping(x,0,100,80,255)
+remap.remap(x,0,100,80,255)
 '''

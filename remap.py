74c4561b6f3ab9dcb04c7f7099793662132663b4
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2021-08-10 09:44:43 +0900

    Update remap.py

--- remap.py
+++ remap.py
@@ -2,16 +2,18 @@ def remap(x,in_min,in_max,out_min,out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 '''
-remap(value,fromLow,fromHigh,toLow,toHigh)
+remap(value,currentlyMin,currentlyMax,convertedMin,convertedMax)
 
 value: 変換したい数値(変数)/Value to convert(variable)
-fromLow: 現在の範囲の下限/minimum of currently range
-fromHigh: 現在の範囲の上限/maximum of currently range
-toLow: 変換後の範囲の下限/minimum of converted range
-toHigh: 変換後の範囲の上限/maximum of converted range
+currentlyMin: 現在の範囲の下限/minimum of currently range
+currentlyMax: 現在の範囲の上限/maximum of currently range
+convertedMin: 変換後の範囲の下限/minimum of converted range
+convertedMax: 変換後の範囲の上限/maximum of converted range
 
 *example*
 import remap
 x=50
 remap.remap(x,0,100,80,255)
+remap.remap(x,1,-1,0,100)
+remap.remap(x,0,100,100,0)
 '''
